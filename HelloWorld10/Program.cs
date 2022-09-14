dfc33e5b9a48ed2c86258cb4275d742d5702134b
Grawsie <ryanhockey@live.ca>
2022-09-14 13:34:30 -0300

    Initial commit

---
+++ HelloWorld10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World number TEN!!!!!!");
+            Console.WriteLine("By Ryan McGrath");
+            Console.ReadKey(true);
+
+        }
+    }
+}
